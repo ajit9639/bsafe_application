914d724fc598b8d3eb9ec72fddb4a9986557fc7e
ajit9639 <61927091+ajit9639@users.noreply.github.com>
2024-02-03 13:08:20 +0530

    deploy to the server

--- Driver/config/conn.php
+++ Driver/config/conn.php
@@ -1,8 +1,13 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'bsafeapp_bsafe');
 
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	}
+if($_SERVER['SERVER_NAME']=="localhost"){
+    $conn = new mysqli('localhost', 'root', '', 'bsafeapp_bsafe');
+}else{
+    $conn = new mysqli('localhost', 'bsafeapp_bsafe_root', 'Bs@fe#2024', 'bsafeapp_bsafe');
+}
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 	
 ?>
